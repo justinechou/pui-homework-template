9155bab39449c323482d27a2509eaca8c8853c4b
justinechou <66896082+justinechou@users.noreply.github.com>
2023-12-03 21:58:04 -0500

    Updated Final Project

---
+++ final-project/app.js
@@ -0,0 +1,14 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
